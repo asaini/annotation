0cdb77b3b5c65a54604c0224862f90a6e98d0a46
asaini <abhi@streamlit.io>
2021-05-14 14:19:24 -0700

    New prototype

--- annotation.py
+++ annotation.py
@@ -6,38 +6,47 @@ from PIL import Image
 script_path = os.path.dirname(__file__)
 rel_path = 'images'
 abs_file_path = script_path + '/' + rel_path
-files = os.listdir(rel_path)
+files = os.listdir(abs_file_path)
 
-state = st.beta_session_state(
-    annotations={},
-    files=files,
-    current_image='cat.1.jpg'
-)
+if 'initialized' not in st.session_state:
+    st.session_state.annotations = {}
+    st.session_state.files = files
+    st.session_state.current_image = 'cat.1.jpg'
+    st.session_state.initialized = True
+
+# state = st.session_state
+
+# state = st.beta_session_state(
+#     annotations={},
+#     files=files,
+#     current_image='cat.1.jpg'
+# )
 
 
-def load_next_image(annotation_response):
-    state.annotations[state.current_image] = annotation_response
-    random_index = random.randint(0, len(state.files) - 1)
-    state.current_image = state.files[random_index]
-    state.files.remove(state.current_image)
+def annotate():
+    annotation_response = st.session_state.annotation_response
+    st.session_state.annotations[st.session_state.current_image] = annotation_response
+    random_index = random.randint(0, len(st.session_state.files) - 1)
+    st.session_state.current_image = st.session_state.files[random_index]
+    st.session_state.files.remove(st.session_state.current_image)
 
 
-image_path = abs_file_path + '/' + state.current_image
+image_path = abs_file_path + '/' + st.session_state.current_image
 image = Image.open(image_path)
 
 st.title('Cat vs Dog: Image Annotation')
 
 st.image(image, caption='Please input whether this is a dog or a cat')
 
-annotation_response = st.selectbox(label='', options=['cat', 'dog'])
-st.button(label='Next', on_click=lambda: load_next_image(annotation_response))
+annotation_response = st.selectbox(key='annotation_response', label='', options=['cat', 'dog'])
+st.button(label='Next', on_change=annotate)
 
 
 st.markdown('---')
 st.header('Annotations')
-st.write(state.__dict__)
+st.write(st.session_state.annotations)
 
 
-if len(state.files) == 0:
+if len(st.session_state.files) == 0:
     st.warning('Finished Annotation')
     st.stop()
